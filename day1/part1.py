12ae3fbb88e63e2c37c1f1e891c230fb1fd138e2
BuildTools <unconfigured@null.spigotmc.org>
2022-12-08 16:31:07 +0200

    day1

---
+++ day1/part1.py
@@ -0,0 +1,17 @@
+with open('input', 'r') as f:
+	lines = f.readlines()
+	lines = [line.strip() for line in lines]
+
+max_sum = 0
+sum = 0
+
+for element in lines:
+	if element:
+		sum += int(element)
+
+	if not element:
+		if sum > max_sum:
+			max_sum = sum
+		sum = 0
+
+print(max_sum)
